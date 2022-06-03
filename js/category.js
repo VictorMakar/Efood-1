11edd192ac8f50d9dc717238f98725d11e9fc694
VictorMakar <makarevich.2019@mail.ru>
2022-06-03 11:02:36 +0300

    sd
    sd

--- js/category.js
+++ js/category.js
@@ -397,6 +397,15 @@ addToCartCheeseburger.addEventListener('click',()=>{
 })
 
 
+const menubtn = document.querySelector('.menu-btn2');
+const menu = document.querySelector('.menu');
+const content = document.querySelector('.content');
+menubtn.onclick = function (){
+  menu.classList.toggle('menu_active')
+  content.classList.toggle('content_active')
+}
+
+
 
 });
 
